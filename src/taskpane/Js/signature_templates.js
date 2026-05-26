496c2203eff178c75fed52dd033f56f9444fb53b
Luis Angel Ramirez Hernande <laramirez@pucmm.edu.do>
2026-05-26 09:45:36 -0400

    Espacio faltante para grado academico

--- src/taskpane/Js/signature_templates.js
+++ src/taskpane/Js/signature_templates.js
@@ -11,7 +11,7 @@ function get_template_A_str(user_info)
 
   str +='<table border="0" cellpadding="1" cellspacing="1"><tbody><tr><td valign="top"><font size="3" color="#17365d" face="Arial">';
   //str +='<strong>'+ user_info.name +'</strong></font>';
-  str +='<strong>'+ user_info.name + (is_valid_data(user_info.GrdoAcad) ? "," + user_info.GrdoAcad : "") +'</strong></font>';
+  str +='<strong>'+ user_info.name + (is_valid_data(user_info.GrdoAcad) ? ", " + user_info.GrdoAcad : "") +'</strong></font>';
   str +='<br><font size="2" face="Arial">'+ user_info.job +'</font><br>';
   str +='<font size="3" color="#17365d" face="Arial">';
   str += is_valid_data(user_info.pronoun) ? "<strong>" + user_info.pronoun : "";
