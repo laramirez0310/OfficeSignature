902cb5ffd836cc8b43149fb49f538372d5be6eea
Luis Angel Ramirez <laramirez@pucmm.edu.do>
2026-08-07 11:06:42 -0400

    Cambios de URL de power automate

--- src/taskpane/Js/signature_templates.js
+++ src/taskpane/Js/signature_templates.js
@@ -13,8 +13,8 @@
       redirect: 'follow'
     };
     
-      //fetch("https://default73c9a419863d4226a83f7a200ad69b.e9.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/15/workflows/8e0dc4da953541c6af3b32bbe54b40e6/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=PRdRmuPRX7vTs8X03RmspDc-7zvXsKkQ7jbRHuvId_E", requestOptions)        
-      fetch("https://default73c9a419863d4226a83f7a200ad69b.e9.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/01/workflows/5bdb62f710c24557b91fee8a539c2534/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ORS8WbddWepHcGZH9uWJqcQwHvIYQ-4saDQ9V8DDcEk",requestOptions)
+      fetch("https://default73c9a419863d4226a83f7a200ad69b.e9.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/15/workflows/8e0dc4da953541c6af3b32bbe54b40e6/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=PRdRmuPRX7vTs8X03RmspDc-7zvXsKkQ7jbRHuvId_E", requestOptions)        
+      //fetch("https://default73c9a419863d4226a83f7a200ad69b.e9.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/01/workflows/5bdb62f710c24557b91fee8a539c2534/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ORS8WbddWepHcGZH9uWJqcQwHvIYQ-4saDQ9V8DDcEk",requestOptions)
       .then(response => response.json())
         .then(result => { dataFirma(result) }) 
         .catch(error => console.log('error', error));
